35eb7a9d23394123f18088e94729e5edb28ca776
JSv4 <scrudato@umich.edu>
2024-04-01 10:54:44 -0500

    Finished (for now) the doc analysis pipeline.

--- examples/llamaindex/main.py
+++ examples/llamaindex/main.py
@@ -6,8 +6,11 @@ from pathlib import Path
 
 from llama_index.legacy.embeddings import HuggingFaceEmbedding
 from llama_index.llms.openai import OpenAI
-from llama_index.core import VectorStoreIndex, ServiceContext, SimpleDirectoryReader
+from llama_index.core import Settings
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core.retrievers import BaseRetriever
 from BotsOnRails import ExecutionTree, node_for_tree
+from examples.llamaindex.display import display_stock_series_cards
 from examples.llamaindex.models import StockSeriesInfo, ParticipationCap
 
 tree = ExecutionTree()
@@ -30,77 +33,99 @@ llm = OpenAI(
     model="gpt-4-0125-preview",
     temperature=0.0
 )
-llamaindex_context = ServiceContext.from_defaults(
-    embed_model=embed_model,
-    llm=llm,
-    chunk_size=4096
-)
+
+# Configure Llama Index
+Settings.chunk_size = 4096
+Settings.llm = llm
+Settings.embed_model = embed_model
 
 
 @node(start_node=True, next_nodes="check_doc_type")
-def load_document(doc_dir: str, **kwargs) -> tuple[str, VectorStoreIndex]:
-    documents = SimpleDirectoryReader("../paul_graham_essay/data").load_data()
+def load_document(doc_dir: str, **kwargs) -> BaseRetriever:
+    print(f"Loading dovs from {doc_dir}...")
+    documents = SimpleDirectoryReader(doc_dir).load_data()
+    print(f"Vectorizing...")
     index = VectorStoreIndex.from_documents(documents)
-    return documents[0].txt, index
+    print(f"Building retriever...")
+    retriever = index.as_retriever(similarity_top_k=3)
+    print("Passing the baton... It's been a pleasure.")
+    ## TODO - check that return signatures match function return signatures...
+    return retriever
 
 
 @node(next_nodes={"incorporation": "extract_stock_info", "other": "end_pipeline"})
-def check_doc_type(doc_text: str, *args, **kwargs) -> str:
-    doc_type = marvin.classify(doc_text, labels=["incorporation", "other"])
+def check_doc_type(retriever: BaseRetriever, *args, **kwargs) -> str:
+    retrieved_context = retriever.retrieve("This document, made between this parties as of this date.")
+    context = "------\n".join([rc.text for rc in retrieved_context])
+    print(f"Doc context: {context}")
+    doc_type = marvin.classify(context, labels=["incorporation", "other"])
+    print(f"Inferred doc type: {doc_type}")
     return doc_type
 
 
-@node(next_nodes=('FOR_EACH',"retrieve_passages"), unpack_output=False)
-def extract_stock_info(doc_text: str, **kwargs) -> List[StockSeriesInfo]:
+@node(next_nodes='filter_common', unpack_output=False)
+def extract_stock_info(*args, **kwargs) -> List[StockSeriesInfo]:
     print(f"extract_stock_info() - runtime kwargs: {kwargs}")
-    index = kwargs['runtime_args']['load_document']['yo']
-    # series_text =
 
-    stock_series_list = marvin.extract(doc_text, target=StockSeriesInfo)
+    retriever = kwargs['runtime_args']['input_chain']['check_doc_type'][0]
+    retrieved_stock_text = retriever.retrieve('Stock or series of stock authorized and/or issued by this company')
+    stock_text = "-----\n".join([rc.text for rc in retrieved_stock_text])
+    stock_series_list = marvin.extract(stock_text, target=StockSeriesInfo)
+    print(f"Found {len(stock_series_list)}")
     return stock_series_list
+
+
+@node(next_nodes=('FOR_EACH', "retrieve_passages"), unpack_output=False)
+def filter_common(stock_series: List[StockSeriesInfo], **kwargs) -> List[StockSeriesInfo]:
+    return [
+        ss for ss in stock_series if ss.stock_class_name.lower() != 'common'
+    ]
 
 
 @node(next_nodes="extract_participation_cap", unpack_output=False)
 def retrieve_passages(stock_series: StockSeriesInfo, **kwargs) -> List[str]:
-    query = f"{stock_series.stock_series_name} participation cap"
-    # passages = index.as_retriever()
+    print(f"retrieve_passages - kwargs: {kwargs}")
+    loop_data = kwargs['runtime_args']['for_each_loop']
+    max_iterations = loop_data['expected']
+    current_index = loop_data['actual']
+    iterating_over = loop_data['source_iterable']
 
-    # return [passage.text for passage in passages]
-    return ["Bob", "is", "my", "uncle"]
+    print(f"Retrieving passes #{current_index} for max iterations {max_iterations}: {iterating_over}")
+
+    retriever = kwargs['runtime_args']['input_chain']['check_doc_type'][0]
+    participation_cap_passages = retriever.retrieve(f"maximum amount the preferred series "
+                                                    f"{stock_series.stock_series_name} is entitled to receive without"
+                                                    f" forced conversion to Common")
+
+    return [pc.text for pc in participation_cap_passages]
 
 
-@node(next_nodes="store_data", wait_for_approval=True)
-def extract_participation_cap(passages: List[str], **kwargs) -> Optional[ParticipationCap]:
-    participation_cap = None
-    for passage in passages:
-        participation_cap = marvin.cast(
-            passage,
-            target=ParticipationCap,
-            instructions=f"Extract participation cap information from the given passage, if available. "
-                         f"If not found, return null.",
-            prefer_null=True,
-        )
-        if participation_cap is not None:
-            break
+@node(next_nodes="aggregate_data", unpack_output=False)
+def extract_participation_cap(passages: List[str], **kwargs) -> tuple[StockSeriesInfo, Optional[ParticipationCap]]:
+    print(f"Extract_participation_cap got iterable data: {kwargs['runtime_args']['for_each_loop']}")
 
-    return participation_cap
+    series_info = kwargs['runtime_args']['input_chain']['retrieve_passages'][0]
+    search_area = "-----\n".join(passages)
+
+    participation_cap = marvin.cast(
+        search_area,
+        target=Optional[ParticipationCap],
+        instructions=f"Extract participation cap information for maximum amount given series of preferred can "
+                     f"receive before converting to common based on provided excerpts from a certificate of "
+                     f"incorporation, if available. If not found, return null."
+    )
+    return series_info, participation_cap
 
 
 @node(aggregator=True)
-def store_data(participation_cap: Optional[ParticipationCap], **kwargs):
-    print("Loading data into the system...")
-    # for stock_series, participation_cap in zip(stock_series_list, participation_caps):
-    #     print(f"Stock Series: {stock_series.stock_series_name}")
-    #     if participation_cap:
-    #         print(f"Participation Cap: {participation_cap.json(indent=2)}")
-    #     else:
-    #         print("No participation cap found for this series.")
-    print("Data loading completed.")
+def aggregate_data(series_data: tuple[StockSeriesInfo, Optional[ParticipationCap]], **kwargs) -> tuple[StockSeriesInfo, Optional[ParticipationCap]]:
+    return series_data
 
 
 @node()
 def end_pipeline(*args, **kwargs):
     print("Document is not a Certificate of Incorporation. Pipeline ended.")
+
 
 # Some docs to process
 doc_dir_names = [
@@ -111,8 +136,10 @@ doc_dir_names = [
 
 # Compile the execution tree
 tree.compile(type_checking=True)
+tree.visualize_via_graphviz()
 
 for doc_dir in doc_dir_names:
     doc_path = my_dir / doc_dir
-    tree.run(doc_path.__str__())
-
+    series_data = tree.run(doc_path.__str__())
+    print(f"\n----------\nSeries data for {doc_dir}:")
+    display_stock_series_cards(series_data)
